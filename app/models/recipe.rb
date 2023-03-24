ca5e692c15deb75506934cb0f363ee2d61da1cd5
Osorabrian <osorabrian@gmail.com>
2023-03-24 06:00:53 +0300

    completed creating models

---
+++ app/models/recipe.rb
@@ -0,0 +1,12 @@
+class Recipe < ApplicationRecord
+
+    belongs_to :user
+
+    validates :title, {
+        presence: true,
+    }
+
+    validates :instructions, {
+        length: {minimum: 50}
+    }
+end
